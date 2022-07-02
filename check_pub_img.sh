c7bb752b3e0c7f30d1677246f0cbb09643b4f2fc
xiantang <zhujingdi1998@gmail.com>
2022-07-03 00:35:50 +0800

    A  .github/workflows/checker.yml A  check_pub_img.sh

---
+++ check_pub_img.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# if valid, return 0, otherwise return exit code 1
+function check_image_link() {
+    link=$1
+    # use curl to check if response code not 200, return 1
+    echo $link
+    wget -q --max-redirect 0 -O /dev/null $link  > /dev/null
+    if [ $? -ne 0 ]; then
+      echo "image link is invalid"
+      exit
+    fi
+}
+
+
+IMAGE_PATH="$(grep -R "\!\[" .  | grep  -v "Binary file" | cut -d ':' -f 2- | sed  's/.*(//' | sed 's/).*$//' | awk '/http/ {print $0}')"
+
+
+for i in $IMAGE_PATH
+do
+  check_image_link $i
+done
+
