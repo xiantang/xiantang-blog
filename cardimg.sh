c2f336cd57a6e368f2d57d5edeba23a6d72d86c3
xiantang <zhujingdi1998@gmail.com>
2023-10-08 19:03:23 +0800

    commit

--- cardimg.sh
+++ cardimg.sh
@@ -1,4 +1,4 @@
 ##!/bin/bash
 
 # bash cardimg.sh how_to_fix_big_interface
-tcardgen -f font   -t template.png content/post/$1.md -o content/post/$1.png
+tcardgen -f font -t template.png content/post/$1.md -o content/post/$1.png
--- sync.sh
+++ sync.sh
@@ -4,10 +4,9 @@
 git config user.email "zhujingdi1998@gmail.com"
 git config user.name "xiantang"
 
-
 git config pull.rebase false
 git add .
-result=`git status -s`
+result=$(git status -s)
 git commit -m "$result"
 git pull origin code
 git push origin code
