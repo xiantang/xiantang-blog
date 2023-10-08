ed3d8e8b3e1cf154f1f429b748c72f2acdf70343
xiantang <zhujingdi1998@gmail.com>
2023-10-08 21:02:23 +0800

    commit

--- translate.sh
+++ translate.sh
@@ -5,7 +5,5 @@ md_to_be_tanslated="$({
 } | sort | uniq -u)"
 
 echo "$md_to_be_tanslated" | while read -r line; do
-	echo "start to translate $line"
 	chatgpt-md-translator -m 4 -f 1000 "$line" --out-suffix=.en.md
-
 done
